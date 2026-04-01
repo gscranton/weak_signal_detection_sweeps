f29b71168ea5cc7eac855f9d478f8f3e31ff02e0
gscranton <greggscranton@gmail.com>
2026-04-01 15:29:26 -0400

    Add files via upload

---
+++ sweeps/sweep1/input_files/max_t_shift_100.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+phi1 = 0.0
+save_verbosity_index = 500
+pulse_filename = '/mnt/sda1/weak_signals/sensor_data/prepped_data/pulse1.csv'
+x10 = 0.028998442540967034
+z10 = -84.42093768595821
+x20 = -0.169103092562322
+z20 = 5.6747251829963075
+x30 = 0.10030426818849783
+z30 = 77.7816827284428
+t_sig_start = 0.0
+noise_flag = True
+gamma1 = 0.7
+gamma2 = 1.1
+alpha1 = 1e4
+alpha2 = 1e4
+beta1 = 0
+C1 = 1.0
+kappa12 = 300
+sig_repeat_timestep_factor = 1
+pulse_width = 1.0/(6.3*15.0)
+pulse_center = 40.0
+sensor_increment = 0
+phi3 = np.pi
+pulse_type = 0
+del_t_ff = 0.004
+t_offset1 = 0
+t_offset2 = 0
+t_offset3 = 0
+input_filename = '/mnt/sda1/mad_data_12-4-25/prepped_data/flights1_mod1.csv'
+A1 = 1e4
+total_t = 30.0
+signal_mag = 0.1
+af_list = '1 100 1000 3000 5000 7000'
+pulse1_height = 0
+pulse1_width = 10/(2*np.sqrt(2*np.log(2)))
+pulse1_type = 1
+pulse1_center = 0
+pulse2_height = 0
+pulse2_width = 10/(2*np.sqrt(2*np.log(2)))
+pulse2_type = 1
+pulse2_center = 140
+pulse3_height = 0
+pulse3_width = 10/(2*np.sqrt(2*np.log(2)))
+pulse3_type = 1
+pulse3_center = 0
+spect_num = 5000
+Cff1 = 0
+Cff2 = 1
+Cff3 = 0
+t_offset2 = 0
+max_t_shift = 100
+num_t_steps = 5
+t_shift_superblock_size = 5
+x10 = -0.35855048462514466
+z10 = 107.86692450322923
+x20 = 0.00010210034673662672
+z20 = -0.00010738032412920047
+x30 = 0.358839140293869
+z30 = -107.86400005602802
+frequency = 0.2
+alpha1 = 0.1
+alpha2 = 1.33
+kappa12 = 0.14
+gamma1 = 1.5
+gamma2 = 0.75
+abs_pulse_location_flag = 1
+snap_flag = 1
+
+
+
+
+
+
+
